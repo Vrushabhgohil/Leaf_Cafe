67190d83286f32369e8518a067db428781131e93
vrushabh <vrushabh160.rejoice@gmail.com>
2024-07-16 18:49:42 +0530

    new added some feature

--- tenants/user_api/api.py
+++ tenants/user_api/api.py
@@ -1,6 +1,8 @@
+from flask_login import current_user, login_required
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from flask import Blueprint, render_template
+from flask import Blueprint, redirect, render_template, request, url_for
+from tenants.users import *
 
 
 user_api  = Blueprint('user_api',__name__,template_folder='templates',static_folder='static')
@@ -9,17 +11,33 @@ engine = create_engine(
 Session = sessionmaker(bind=engine)
 session = Session() 
 
-@user_api.route('/login')
+@user_api.route('/login',methods=['GET','POST'])
 def login():
+    if request.method == 'POST':
+        Login_user()
+        if Login_user():
+            return redirect(url_for('user_api.profile'))
+        else:
+            msg = "invalid email or password!!"    
+            return render_template('user/login.html',msg=msg)
+        
     return render_template('user/login.html')
 
-@user_api.route('/signup')
+@user_api.route('/signup',methods=['GET','POST'])
 def signup():
+    if request.method == 'POST':
+        Signup_user()
+        return redirect(url_for('user_api.login'))
     return render_template('user/signup.html')
 
 @user_api.route('/home')
 def home():
-    return "Hello World"
+    return render_template('user/home.html')
+
+@user_api.route('/profile')
+def profile():
+    one_user = User.query.filter_by(name='sam').first()
+    return render_template('user/profile.html',one_user=one_user)
 
 @user_api.route('/logout')
 def logout():
--- tenants/users.py
+++ tenants/users.py
@@ -1,1 +1,37 @@
 # All The Function Related to the User it will be here
+
+import uuid
+import os
+from werkzeug.utils import secure_filename
+from flask import redirect, render_template, request, url_for
+from common.database import db
+from common.model import User
+
+
+def Signup_user():
+    id = str(uuid.uuid4())
+    name = request.form.get('name')
+    dp_addr_file = request.files.get('dp_addr_file')
+    email = request.form.get('email')
+    phone = request.form.get('phone')
+    password = request.form.get('password')
+    filename = None
+    if dp_addr_file:
+        filename = secure_filename(dp_addr_file.filename)
+        dp_addr_file.save(os.path.join('E:\\vrushabh\\Project\back_end\static\\users_image',filename))
+
+    add_user = User(id=id,name=name,dp_addr=filename,email=email,phone=phone,password=password)
+    db.session.add(add_user)
+    db.session.commit()
+
+def Login_user():
+    email = request.form.get('email')
+    password = request.form.get('password')    
+
+    user = User.query.filter_by(email=email).first()
+    if user:
+        if user.password == password and user.email == email:
+            user.is_active = True
+            db.session.commit()
+            return True
+    return False
